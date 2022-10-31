2206558f82e088f019e38a379b294d9be72fe6a3
Luana Stefania Fernandez <luanastefania2@gmail.com>
2022-10-31 11:10:26 -0300

    modificación cálculos función valor final

--- trabajo de investigación/eventos.js
+++ trabajo de investigación/eventos.js
@@ -1,41 +1,39 @@
 //Deberá contener la funcionalidad en Javascript, al momento de presionar el botón “Resumen”, deberá mostrar en la sección “Total a Pagar: $”, el monto correspondiente a la cantidad de tickets a comprar con el descuento correspondiente dependiendo la categoría seleccionada, existen 3 categorías, Estudiante, Trainee, Junior
+const btnResumen = document.getElementById("btnResumen");
+const hTotalApagar = document.getElementById("totalApagar");
 
+function valorFinal() {
 
-function valorTotal() {
-
-    let cantTickets;
+    let cantidadTickets;
     let categoria;
-    let total;
     let valorEst;
     let valorTrain;
     let valorJun;
 
-    cantTickets = document.getElementById(`autoSizingInput`). value;
-    categoria = categoria= document.getElementById(`autoSizingSelect`). value;
+    cantidadTickets = document.getElementById(`cantTickets`).value;
+    //console.log(cantidadTickets)
+    categoria = document.getElementById(`categoria`).value;
+    //console.log(categoria)
 
-    console.log(cantTickets + categoria);
+    valorEst = 40 * cantidadTickets;
+    //console.log(valorEst)
+    valorTrain = 100 * cantidadTickets;
+    //console.log(valorTrain)
+    valorJun = 170 * cantidadTickets;
+    //console.log(valorJun)
 
-    valorEst = 40;
-    valorTrain = 100;
-    valorJun = 170;
 
-    if (categoria == "E" && categoria !="T" && categoria != "J") {
-        total = valorEst * cantTickets;
-        document.getElementById(`totalApagar`).innerHTML = `Total a pagar` + total;
-    } else {
-        if (categoria == "T" && categoria != "E" && categoria != "J") {
-            total = valorTrain * cantTickets;
-            getElementById(`totalApagar`). innerHTML = `Total a pagar` + total;
-        } else {
-            total = valorJun * cantTickets; 
-            document.getElementById(`totalApagar`).innerHTML = `Total a pagar` + total;
+        if (categoria == `Estudiante`) {
+            document.getElementById(`totalApagar`).innerHTML = `Total a pagar $ ` + valorEst;
+        } else if (categoria == `Trainee`) {
+            document.getElementById(`totalApagar`).innerHTML = `Total a pagar $ ` + valorTrain;
+        } else if (categoria == `Junior`) {
+            document.getElementById(`totalApagar`).innerHTML = `Total a pagar $ ` + valorJun;
         }
     }
 
+ btnResumen.onclick = valorFinal;
+
+function borrar(){
+    document.getElementById()
 }
-
-
-
-
-
-
